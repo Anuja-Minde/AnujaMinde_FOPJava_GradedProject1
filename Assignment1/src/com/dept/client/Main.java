49990215e017bd70dbac15a1e19e999c11678145
Anuja-Minde <111695523+Anuja-Minde@users.noreply.github.com>
2022-10-04 17:39:49 +0530

    Update Main.java

--- Assignment1/src/com/dept/client/Main.java
+++ Assignment1/src/com/dept/client/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		AdminDepartment admin  = new AdminDepartment();
-		System.out.println("Welcome to " + admin.departmentName());
+		System.out.println(" Welcome to " + admin.departmentName());
 		System.out.println(admin.getTodaysWork());
 		System.out.println(admin.getWorkDeadline());
 		System.out.println(admin.isTodayAHoliday());
@@ -15,7 +15,7 @@ public class Main {
 		System.out.println();
 		
 		HrDepartment hr  = new HrDepartment();
-		System.out.println("Welcome to " + hr.departmentName());
+		System.out.println(" Welcome to " + hr.departmentName());
 		System.out.println(hr.doActivity());
 		System.out.println(hr.getTodaysWork());
 		System.out.println(hr.getWorkDeadline());
@@ -24,13 +24,12 @@ public class Main {
 		System.out.println();
 		
 		TechDepartment tech  = new TechDepartment();
-		System.out.println("Welcome to " + tech.departmentName());
+		System.out.println(" Welcome to " + tech.departmentName());
 		System.out.println(tech.getTodaysWork());
 		System.out.println(tech.getWorkDeadline());
 		System.out.println(tech.getTechStackInformation());
 		System.out.println(tech.isTodayAHoliday());
 		
-
 	}
 
 }
